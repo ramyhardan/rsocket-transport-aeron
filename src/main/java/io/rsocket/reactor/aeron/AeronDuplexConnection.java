d80efa6116cfbde311a5c3f9e1bb9064945ccff9
segabriel <sergiy.gabriel@jivygroup.com>
2018-12-13 17:47:35 +0200

    Fixed buffer mapping

--- src/main/java/io/rsocket/reactor/aeron/AeronDuplexConnection.java
+++ src/main/java/io/rsocket/reactor/aeron/AeronDuplexConnection.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.aeron.Connection;
 import reactor.core.Disposable;
-import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -19,10 +18,8 @@ public class AeronDuplexConnection implements DuplexConnection {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AeronDuplexConnection.class);
 
-  private final EmitterProcessor<ByteBuffer> processor = EmitterProcessor.create();
   private final Connection connection;
   private final Disposable channelClosed;
-  private final Disposable outboundDisposable;
 
   public AeronDuplexConnection(Connection connection) {
     this.connection = connection;
@@ -36,44 +33,22 @@ public class AeronDuplexConnection implements DuplexConnection {
                   }
                 })
             .subscribe();
-
-    this.outboundDisposable =
-        connection
-            .outbound()
-            .send(processor)
-            .then()
-            .subscribe(
-                null,
-                th -> {
-                  LOGGER.warn("outbound of {} was failed with error: {}", this, th);
-                  dispose();
-                },
-                this::dispose);
   }
 
   @Override
   public Mono<Void> send(Publisher<Frame> frames) {
-
-    return Mono.create(
-        sink ->
-            Flux.from(frames)
-                .map(
-                    frame -> {
-                      ByteBuffer buffer = frame.content().nioBuffer();
-                      ReferenceCountUtil.safeRelease(frame);
-                      return buffer;
-                    })
-                .subscribe(processor::onNext, sink::error, sink::success));
-
-//    return Flux.from(frames)
-//        .map(
-//            frame -> {
-//              ByteBuffer buffer = frame.content().nioBuffer();
-//              ReferenceCountUtil.safeRelease(frame);
-//              return buffer;
-//            })
-//        .flatMap(buffer -> connection.outbound().send(Mono.just(buffer)).then())
-//        .then();
+    return Flux.from(frames)
+        .map(
+            frame -> {
+              ByteBuffer buffer = frame.content().nioBuffer();
+              ByteBuffer buffer1 = ByteBuffer.allocateDirect(buffer.capacity());
+              buffer1.put(buffer);
+              buffer1.flip();
+              ReferenceCountUtil.safeRelease(frame);
+              return buffer1;
+            })
+        .flatMap(buffer -> connection.outbound().send(Mono.just(buffer)).then())
+        .then();
   }
 
   @Override
@@ -104,7 +79,6 @@ public class AeronDuplexConnection implements DuplexConnection {
 
   @Override
   public void dispose() {
-    outboundDisposable.dispose();
     connection.dispose();
   }
 
